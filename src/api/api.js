1d2266cd630301a26091d76b676ffc8edcf563fd
Inderajeet <inderajeetperiyasami210@gmail.com>
2026-01-19 11:33:45 +0530

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/admin';
+// const API_BASE_URL = 'http://localhost:5000/api/admin';
+const API_BASE_URL = 'https://housing-backend.vercel.app/api/admin';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
